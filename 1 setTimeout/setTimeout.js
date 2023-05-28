614d764a3dc4248a771b7cc1a52f5f63e0d8732b
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-05-28 17:49:38 -0400

    setTimeout

---
+++ 1 setTimeout/setTimeout.js
@@ -0,0 +1,8 @@
+console.log("I am at the beginning of the code");
+
+setTimeout(
+  () => console.log("I am in the setTimeout callback function"),
+  "600"
+);
+
+console.log("I am at the end of the code");
