3a214c04e89f0d5deb1b0755a528e946cd7b07e5
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-06-03 21:05:32 -0400

    limited interval

---
+++ 4 limitedInterval/limitedInterval.js
@@ -0,0 +1,11 @@
+const limitedInterval = (callback, wait, limit) => {
+  let waittime = wait;
+  while (waittime < limit) {
+    setTimeout(function () {
+      callback();
+    }, wait);
+    waittime += wait;
+  }
+};
+
+limitedInterval(() => console.log("repeating"), 100, 550); // should log 'repeating' once per 100 ms, five times.
